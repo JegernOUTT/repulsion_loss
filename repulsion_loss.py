55170576c6712929a7a8b7638c8a26ca7ec4205c
s-vakhreev <svakhreev@gmail.com>
2018-04-26 13:31:58 +0300

    Changed calculation method, added some coeff

--- repulsion_loss.py
+++ repulsion_loss.py
@@ -49,8 +49,8 @@ def bbox_iog(ground_truth, predicted):
     return tf.transpose(tf.maximum(intersect_area / gt_area, 0.0), (1, 0, 2))
 
 
-def smooth_l1_distance(y_true, y_pred, delta=0.5):
-    sigma_squared = delta ** 2
+def smooth_l1_distance(y_true, y_pred, smooth):
+    sigma_squared = smooth ** 2
     regression_diff = y_pred - y_true
     regression_diff = tf.abs(regression_diff)
     return tf.where(
@@ -60,109 +60,91 @@ def smooth_l1_distance(y_true, y_pred, delta=0.5):
     )
 
 
-def smooth_ln(x, delta):
+def smooth_ln(x, smooth):
     return tf.where(
-        tf.less_equal(x, delta),
+        tf.less_equal(x, smooth),
         -tf.log(1 - x),
-        ((x - delta) / (1 - delta)) - tf.log(1 - delta)
+        ((x - smooth) / (1 - smooth)) - tf.log(1 - smooth)
     )
 
 
-def attraction_term(y_true, y_pred, ious_over_truth_boxes):
-    # Найти из y_true бокс с большим IOU для всех y_pred
-    # Прогоняем его через smooth_l1
-    # Суммиируем
-    # Делим на количество y_pred
-
+def attraction_term(y_pred, ious_over_truth_boxes, smooth=0.5):
     iou_max_indices = tf.argmax(ious_over_truth_boxes[..., 0], axis=2)
     ious_over_truth_boxes = ious_over_truth_boxes[..., 1:]
 
-    pred_boxes_with_max_ious = None
+    gt_boxes_with_max_ious = None
     for batch_num in np.arange(y_pred.shape[0].value, dtype=np.int64):
-        indices = tf.stack([tf.cast(tf.tile([batch_num], [tf.shape(y_true)[1]]), dtype=tf.int64),
-                            tf.range(tf.cast(tf.shape(y_true)[1], dtype=tf.int64)),
+        indices = tf.stack([tf.cast(tf.tile([batch_num], [tf.shape(y_pred)[1]]), dtype=tf.int64),
+                            tf.range(tf.cast(tf.shape(y_pred)[1], dtype=tf.int64)),
                             tf.cast(iou_max_indices[batch_num], dtype=tf.int64)])
         indices = tf.transpose(indices)
 
-        if pred_boxes_with_max_ious is None:
-            pred_boxes_with_max_ious = tf.expand_dims(tf.gather_nd(ious_over_truth_boxes, indices), axis=0)
+        if gt_boxes_with_max_ious is None:
+            gt_boxes_with_max_ious = tf.expand_dims(tf.gather_nd(ious_over_truth_boxes, indices), axis=0)
         else:
             boxes_with_max_ious = tf.expand_dims(tf.gather_nd(ious_over_truth_boxes, indices), axis=0)
-            pred_boxes_with_max_ious = tf.concat([pred_boxes_with_max_ious, boxes_with_max_ious], axis=0)
+            gt_boxes_with_max_ious = tf.concat([gt_boxes_with_max_ious, boxes_with_max_ious], axis=0)
 
-    l1_distances = smooth_l1_distance(y_true, pred_boxes_with_max_ious)
-    return tf.reduce_sum(tf.cast(l1_distances, tf.float32)) / tf.cast(tf.shape(y_true)[1], dtype=tf.float32)
+    l1_distances = smooth_l1_distance(y_pred, gt_boxes_with_max_ious, smooth)
+    return tf.reduce_sum(tf.cast(l1_distances, tf.float32)) / tf.cast(tf.shape(y_pred)[1], dtype=tf.float32)
 
 
-def repulsion_term_gt(y_true, y_pred, ious_over_truth_boxes, alpha):
-    # Найти из y_true бокс с вторым по величине IOU
-    # Находим IoG между этим боксом и y_true
-    # Прогоняем IoG через smooth_ln
-    # Суммиируем
-    # Делим на количество y_pred
-
-    alpha = tf.Variable(alpha, dtype=tf.float32)
-
+def repulsion_term_gt(y_pred, ious_over_truth_boxes, smooth=0.99):
     _, indices_2highest_iou = tf.nn.top_k(ious_over_truth_boxes[..., 0], k=2)
     ious_over_truth_boxes = ious_over_truth_boxes[..., 1:]
     indices_2highest_iou = indices_2highest_iou[..., 1]
 
-    pred_boxes_with_max_ious = None
+    gt_boxes_with_max_ious = None
     for batch_num in np.arange(y_pred.shape[0].value, dtype=np.int64):
-        indices = tf.stack([tf.cast(tf.tile([batch_num], [tf.shape(y_true)[1]]), dtype=tf.int64),
-                            tf.range(tf.cast(tf.shape(y_true)[1], dtype=tf.int64)),
+        indices = tf.stack([tf.cast(tf.tile([batch_num], [tf.shape(y_pred)[1]]), dtype=tf.int64),
+                            tf.range(tf.cast(tf.shape(y_pred)[1], dtype=tf.int64)),
                             tf.cast(indices_2highest_iou[batch_num], dtype=tf.int64)])
         indices = tf.transpose(indices)
 
-        if pred_boxes_with_max_ious is None:
-            pred_boxes_with_max_ious = tf.expand_dims(tf.gather_nd(ious_over_truth_boxes, indices), axis=0)
+        if gt_boxes_with_max_ious is None:
+            gt_boxes_with_max_ious = tf.expand_dims(tf.gather_nd(ious_over_truth_boxes, indices), axis=0)
         else:
             boxes_with_max_ious = tf.expand_dims(tf.gather_nd(ious_over_truth_boxes, indices), axis=0)
-            pred_boxes_with_max_ious = tf.concat([pred_boxes_with_max_ious, boxes_with_max_ious], axis=0)
+            gt_boxes_with_max_ious = tf.concat([gt_boxes_with_max_ious, boxes_with_max_ious], axis=0)
 
-    ln_distances_for_iog = smooth_ln(bbox_iog(y_true, pred_boxes_with_max_ious), alpha)
-    return tf.reduce_sum(ln_distances_for_iog) / tf.cast(tf.shape(y_true)[1], dtype=tf.float32)
+    ln_distances_for_iog = smooth_ln(bbox_iog(gt_boxes_with_max_ious, y_pred), smooth)
+    return tf.reduce_sum(ln_distances_for_iog) / tf.cast(tf.shape(y_pred)[1], dtype=tf.float32)
 
 
-def repulsion_term_box(y_pred, betta):
-    # Делим все множество y_pred боксов на бокс + цель
-    # (Проходимся циклом и оставляем для каждой y_true бокс из y_pred с наибольшим IoU)
-    # Находим IoU для каждой пары сочетания (Bi, Bj)
-    # Для каждой пары находим отношение smooth_ln(IoU) / IoU + e
-    # Суммиируем
+def repulsion_term_box(ious, smooth=0.01):
+    iou_over_predicted_indices = tf.where(tf.less(ious, 1.0))
+    ious = tf.gather_nd(ious, iou_over_predicted_indices)
 
-    iou_over_predicted = bbox_overlap_iou(y_pred, y_pred)
-    iou_over_predicted_indices = tf.where(tf.less(iou_over_predicted, 1.0))
-    iou_over_predicted = tf.gather_nd(iou_over_predicted, iou_over_predicted_indices)
-
-    dist_sum = tf.reduce_sum(smooth_ln(iou_over_predicted, betta))
-    iou_sum = tf.reduce_sum(iou_over_predicted)
+    dist_sum = tf.reduce_sum(smooth_ln(ious, smooth))
+    iou_sum = tf.reduce_sum(ious)
 
     return dist_sum / tf.maximum(iou_sum, 0.000001)
 
 
-def create_repulsion_loss(objectness_score_treshold=0.5, alpha=0.5, betta=0.5):
+def create_repulsion_loss(alpha=0.5, betta=0.5):
     def _filter_predictions(y_pred):
-        y_pred_indices = tf.where(tf.greater_equal(y_pred[..., 4], objectness_score_treshold))
+        y_pred_indices = tf.where(tf.greater_equal(y_pred[..., 4], 0.5))
         return tf.gather_nd(y_pred, [y_pred_indices])
 
     def _preprocess_inputs(y_true, y_pred):
         return y_true[..., :4], y_pred[..., :4]
 
     def _repulsion_impl(y_true, y_pred):
-        len_of_gt = tf.shape(y_true)[1]
+        len_of_predicted = tf.shape(y_pred)[1]
 
-        iou_over_gt = bbox_overlap_iou(y_true, y_pred)
-        tiled_for_concat = tf.tile(tf.expand_dims(y_pred, axis=1), [1, len_of_gt, 1, 1])
-        highest_iou = tf.concat([tf.expand_dims(iou_over_gt, axis=3), tiled_for_concat], axis=3)
+        ious = bbox_overlap_iou(y_pred, y_true)
+        tiled_for_concat = tf.tile(tf.expand_dims(y_true, axis=1), [1, len_of_predicted, 1, 1])
+        ious_over_truth_boxes = tf.concat([tf.expand_dims(ious, axis=3), tiled_for_concat], axis=3)
 
         return tf.reduce_sum([
-            attraction_term(y_true, y_pred, highest_iou),
-            repulsion_term_gt(y_true, y_pred, highest_iou, alpha),
-            repulsion_term_box(y_pred, betta)
+            attraction_term(y_pred, ious_over_truth_boxes),
+            alpha * repulsion_term_gt(y_pred, ious_over_truth_boxes),
+            betta * repulsion_term_box(ious)
         ])
 
     def _repulsion_loss(y_true, y_pred):
+        # Фильтруем y_pred, оставляя те, у которых IOU > 0,5 хотябы с одним y_true
+
         y_pred = _filter_predictions(y_pred)
         y_true, y_pred = _preprocess_inputs(y_true, y_pred)
 
